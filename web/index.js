f24e973a6a6a7dcb7fa50711d506647e87e3a813
aunz <aunz10@yahoo.com>
2019-01-22 18:16:53 -0500

    refact codes

--- web/index.js
+++ web/index.js
@@ -2,17 +2,21 @@
 // import * as posenet from '@tensorflow-models/posenet'
 
 
-
-const w = 320
+const w = 320 // video width & height
 const h = w * 3 / 4
-let stop = false
-
-const minPoseConfidence = 0.1
-const minPartConfidence = 0.5
 
 const imageScaleFactor = 1 // 0 ~ 1, higher: better accuracy but lower speed
 const flipHorizontal = true // because fed thru webcam
 const outputStride = 16 // 8, 16, 32; higher: better accuracy & lower speed
+
+const minPoseConfidence = 0.1
+const minPartConfidence = 0.5
+
+const state = {
+  color: '#2ECC40', // green 
+  stopped: false,
+}
+
 
 navigator.mediaDevices.getUserMedia({
   audio: false,
@@ -23,51 +27,54 @@ navigator.mediaDevices.getUserMedia({
   },
 }).then(async function (stream) {
 
-    const player = document.getElementById('player')
-    player.width = w
-    player.height = h
+  // set up video
+  const video = document.getElementById('video')
+  video.width = w
+  video.height = h
+  video.srcObject = stream
+  video.onloadedmetadata = () => { video.play() } // set up canvas
 
-    player.srcObject = stream
+  const canvas = document.getElementById('canvas')
+  canvas.width = w
+  canvas.height = h
 
-    await new Promise(resolve => {
-      player.onloadedmetadata = () => { resolve(player) }
-    })
-    player.play()
-    // window.player = player
+  // context for the canvas
+  const ctx = canvas.getContext('2d')
 
-    const canvas = document.getElementById('myCanvas')
-    canvas.width = w
-    canvas.height = h
-    const ctx = canvas.getContext('2d')
-
-    const net = await posenet.load()
+  // load the model
+  posenet.load().then(net => {
     console.log('posenet loaded')
+    
+    detectPose(net, video, ctx)
 
-    async function detectPose() {
-      if (!stop) {
-        const poses = await net.estimateSinglePose(player, imageScaleFactor, flipHorizontal, outputStride)
-        ctx.clearRect(0, 0, w, h)
-        ctx.save()
-        ctx.scale(-1, 1)
-        ctx.translate(-w, 0)
-        ctx.drawImage(player, 0, 0, w, h)
-        ctx.restore()
-
-        drawKeypoints(poses.keypoints, minPartConfidence, ctx)
-        
-      }
-      requestAnimationFrame(detectPose)
-      
-    }
-
-    detectPose()
-
-
+    const controller = document.getElementById('control')
+    controller.textContent = 'STOP'
+    controller.addEventListener('click', function (e) {
+      const nextState = !state.stopped
+      e.currentTarget.textContent = nextState ? 'START' : 'STOP'
+      state.stopped = nextState
+    })
   })
 
-document.getElementById('control').addEventListener('click', function () {
-  stop = !stop
 })
+
+async function detectPose(net, video, ctx) {  
+  if (!state.stopped) {
+    const poses = await net.estimateSinglePose(video, imageScaleFactor, flipHorizontal, outputStride)
+    ctx.clearRect(0, 0, w, h)
+    ctx.save()
+    ctx.scale(-1, 1)
+    ctx.translate(-w, 0)
+    ctx.drawImage(video, 0, 0, w, h)
+    ctx.restore()
+
+    drawKeypoints(poses.keypoints, minPartConfidence, ctx)
+    
+  }
+  requestAnimationFrame(() => detectPose(net, video, ctx))
+  
+}
+
 
 function drawKeypoints(keypoints, minConfidence, ctx, scale = 1) {
   for (let i = 0; i < keypoints.length; i++) {
@@ -75,14 +82,10 @@ function drawKeypoints(keypoints, minConfidence, ctx, scale = 1) {
 
     if (keypoint.score < minConfidence) continue
     const { y, x } = keypoint.position
-    drawPoint(ctx, y * scale, x * scale, 3, 'green')
+
+    ctx.beginPath()
+    ctx.arc(x * scale, y * scale, 3, 0, 2 * Math.PI)
+    ctx.fillStyle = state.color
+    ctx.fill()
   }
-}
-
-
-function drawPoint(ctx, y, x, r, color) {
-  ctx.beginPath()
-  ctx.arc(x, y, r, 0, 2 * Math.PI)
-  ctx.fillStyle = color
-  ctx.fill()
 }
