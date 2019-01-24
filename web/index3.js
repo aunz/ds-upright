4f31f91e286706317dffcc6d7185606778bea82b
aunz <aunz10@yahoo.com>
2019-01-24 10:37:08 -0500

    to extract pose from images

---
+++ web/index3.js
@@ -0,0 +1,28 @@
+// import '@tensorflow/tfjs'
+// import * as posenet from '@tensorflow-models/posenet'
+
+
+const w = 480 // img width & height
+const h = w * 9 / 16
+
+const imageScaleFactor = 1 // 0 ~ 1, higher: better accuracy but lower speed
+const flipHorizontal = true // because fed thru webcam
+const outputStride = 16 // 8, 16, 32; higher: better accuracy & lower speed
+
+const img = document.getElementById('img')
+img.width = w
+img.height = h
+  
+// load the model
+posenet.load().then(async net => {
+  console.log('posenet loaded')
+  
+  detectPose(net, img)
+})
+
+// })
+
+async function detectPose(net, imageSrc) {  
+  const poses = await net.estimateSinglePose(imageSrc, imageScaleFactor, flipHorizontal, outputStride)
+  console.log(poses)
+}
