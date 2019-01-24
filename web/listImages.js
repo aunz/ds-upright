c5da5990c5c01087557a28d4b493d6d1642ca319
aunz <aunz10@yahoo.com>
2019-01-24 10:51:29 -0500

    add express server to serve images

---
+++ web/listImages.js
@@ -0,0 +1,18 @@
+const { readdirSync } = require('fs')
+const express = require('express')
+const app = express()
+
+const filesA = readdirSync('../data/a')
+const filesB = readdirSync('../data/b')
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
+app.use(express.static('../data/'))
+
+app.get('/a', (req, res) => { res.send(filesA) })
+app.get('/b', (req, res) => { res.send(filesB) })
+
+app.listen(3000, () => { console.log('Server is up') })
