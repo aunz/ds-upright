f63f21de0ba836255163237404bb7d517e8285f6
aunz <aunz10@yahoo.com>
2019-01-30 17:06:34 -0500

    refact code

--- web/index6.js
+++ web/index6.js
@@ -17,38 +17,47 @@ function predict(keypoints) {
   return
 }
 
-;(async function () {
-
-  const net = await posenet.load()
-
-  console.log('Ready')
-
-  const poses = await net.estimateSinglePose(document.getElementById('img'), imageScaleFactor, flipHorizontal, outputStride)
-  window.poses = poses
-
-  const model = await tf.loadModel('http://localhost:3000/trained_models/model_cnn_2/model.json')
-
-  // console.log(model)
-
-  const h = 135
+async function predict(model, keypoints) {
+  const h = 135 // h & width of images
   const w = 240
-  // let img = new Array(h).fill(0).map(() => new Array(w).fill(0).map(() => [0]))
-  let img = []
+
+  // const img = new Array(h).fill(0).map(() => new Array(w).fill(0).map(() => [0]))
+  const img = [] // img pixels based on keypoints
   for (let i = -1; ++i < h;) {
     img[i] = []
     for (let j = -1; ++j < w;) img[i][j] = [0]
   }
-  poses.keypoints.forEach(p => {
+  
+  keypoints.forEach(p => {
     let { x, y } = p.position
     x = Math.min(Math.round(x / 2), w - 1)
     y = Math.min(Math.round(y / 2), h - 1)
     img[y][x] = [1]
   })
 
-  img = tf.tensor3d(img)
+  const ts = tf.tensor3d(img).expandDims(0) // turn img pixels into tf array and extend 1 more dim
+  return model.predict(ts).data()
 
-  const result = model.predict(img.expandDims(0))
-  console.log(result.dataSync())
+}
+
+;(async function () {
+
+  const net = await posenet.load()
+  console.log('Net Ready')
+
+  const model = await tf.loadModel('http://localhost:3000/trained_models/model_cnn_2/model.json')
+  console.log('Model Ready')
+
+  // console.time(0)
+  const poses = await net.estimateSinglePose(document.getElementById('img'), imageScaleFactor, flipHorizontal, outputStride)
+  // console.timeEnd(0)
+
+  // console.time(1)
+  const result = await predict(model, poses.keypoints)
+  // console.timeEnd(1)
+  console.log(result[0])
+
+  // test for more images
 
 }())
 
